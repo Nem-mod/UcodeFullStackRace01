308639572ead00788a5c2f8bbe6e88bd70a530c8
northwind08 <zseveryn.lipovetskyz@gmail.com>
2023-10-01 16:17:33 +0300

    FEAT: added 16 cards

--- db/init/cards.sql
+++ db/init/cards.sql
@@ -12,4 +12,18 @@ CREATE TABLE IF NOT EXISTS cards(
 
 INSERT INTO cards(card_type, card_power, card_cost, card_hitpoints, card_name, card_img_url) VALUES
     ('defense', 5, 2, 6, 'Captain America', ''),
-    ('')
+    ('attack', 5, 2, 4, 'Hulk', ''),
+    ('attack', 7, 3, 5, 'Thor', ''),
+    ('defense', 4, 2, 5, 'Black Panther', ''),
+    ('attack', 4, 1, 4, 'Spider Man', ''),
+    ('defense', 7, 3, 7, 'Doctor Strange', ''),
+    ('attack', 6, 2, 6, 'Iron Man', ''),
+    ('attack', 8, 3, 9, 'Thanos', ''),
+    ('defense', 4, 1, 5, 'Scarlet Witch', ''),
+    ('defense', 3, 1, 4, 'Vision', ''),
+    ('attack', 5, 2, 5, 'Venom', ''),
+    ('attack', 7, 3, 7, 'Ultron', ''),
+    ('defense', 4, 1, 6, 'She Hulk', ''),
+    ('defense', 4, 2, 8, 'Ant-Man', ''),
+    ('attack', 4, 1, 5, 'Deathlock', ''),
+    ('attack', 8, 3, 9, 'Spider-Man 2099', '');
